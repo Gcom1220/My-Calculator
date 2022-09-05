c7b622f105eb91b8c901aee86b3ce63bcf54b24d
gcom1220 <wenmalcolm@yahoo.com>
2022-09-05 23:03:11 +0100

    finished

---
+++ calculator.js
@@ -0,0 +1,28 @@
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'AC':
+                display.innerText = '';
+                break;
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = 'Error!';
+                }
+                break;
+            case 'X²':
+                display.innerText = eval(display.innerText*display.innerText);
+                break;
+            case '+/-':
+                display.innerText += '-';
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    })
+});
